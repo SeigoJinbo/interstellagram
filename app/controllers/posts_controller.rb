c012b86e4de02bf04019399f385605f2840ec871
Seigo Jinbo <seigojinbo@gmail.com>
2021-02-01 12:03:11 +0900

    post validates image presence true. flash notice added in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to post_path(@post)
     else
+      flash[:notice] = 'you need to upload an image'
       render 'posts/new'
     end
   end
