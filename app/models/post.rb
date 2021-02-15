ba08827600e49ef82924d7836aeafd05497515b6
Seigo Jinbo <seigojinbo@gmail.com>
2021-02-15 11:31:41 +0900

    create hashtag on post update

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
   has_many :hash_tags, through: :post_hash_tags
   has_many_attached :images, dependent: :destroy
   validates :images, presence: true
-  after_commit :create_hash_tags, on: :create
+  after_commit :create_hash_tags, on: %i[create update]
 
   def create_hash_tags
     extract_name_hash_tags.each { |name| hash_tags.create(name: name) }
