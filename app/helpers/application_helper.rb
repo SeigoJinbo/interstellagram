cbd4867a03c61ee63fbff95719cbf999cced789d
Seigo Jinbo <seigojinbo@gmail.com>
2021-02-18 17:16:56 +0900

    helper method for time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,39 @@ module ApplicationHelper
     end
     return @avatar
   end
+
+  def ago(date)
+    sec = Time.now - date
+
+    if sec < 60
+      return 'less than a minute ago'
+    elsif sec <= 3600
+      return "#{min = (sec / 60).floor} #{min == 1 ? 'minute' : 'minutes'} ago"
+    elsif sec <= 86_400
+      return "#{hour = (sec / 3600).floor} #{hour == 1 ? 'hour' : 'hours'} ago"
+    elsif sec <= 604_800
+      return "#{day = (sec / 86_400).floor} #{day == 1 ? 'day' : 'days'} ago"
+    else
+      return date.to_time.strftime('%b %e %Y')
+    end
+  end
+  def short_ago(date)
+    sec = Time.now - date
+
+    if sec < 60
+      return '<1m'
+    elsif sec <= 3600
+      return "#{min = (sec / 60).floor}m"
+    elsif sec <= 86_400
+      return "#{hour = (sec / 3600).floor}h"
+    elsif sec <= 604_800
+      return "#{day = (sec / 86_400).floor}d"
+    elsif sec <= 2_419_200
+      return "#{day = (sec / 604_800).floor}w"
+    elsif sec <= 29_030_400
+      return "#{day = (sec / 2_419_200).floor}m"
+    else
+      return "#{day = (sec / 29_030_400).floor}y"
+    end
+  end
 end
